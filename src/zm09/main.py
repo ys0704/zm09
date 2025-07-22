27b87137d389f91b0d5a55c02ea9d91faab73de1
YusukeSakai <yuusuke_74_spk@yahoo.co.jp>
2025-07-22 12:25:21 +0900

    add nice commands

---
+++ src/zm09/main.py
@@ -0,0 +1,39 @@
+import datetime
+
+import typer
+
+from zm09 import mathtools
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    A Collection of Useful Commands
+    """
+
+
+@app.command()
+def now():
+    """
+    Show local date and time
+    """
+    today = datetime.datetime.today()
+    typer.echo(today.strftime('%A, %B %d, %Y'))
+
+
+@app.command()
+def gcd(x: int, y: int):
+    """
+    Greatest Common Divisor
+    """
+    typer.echo(mathtools.gcd(x, y))
+
+
+@app.command()
+def divisors (x: int):
+    """
+    Divisors of an integer
+    """
+    typer.echo(mathtools.divisors(x))
